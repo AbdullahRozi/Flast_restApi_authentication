a0fa46af8ca2041ec0cbce4b3a90584ce7d48954
Abdullah Rozi Muhammad <abdullahrozi.muhammad@gmail.com>
2026-06-23 22:34:17 -0700

    Refactor app to use SQLAlchemy and bcrypt for authentication

--- app.py
+++ app.py
@@ -1,22 +1,37 @@
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import bcrypt
 import jwt
 import time
 
 app = Flask(__name__)
-SERVER_SECRET_KEY = "5973598a5b9cba6ebb36d2f95019442d2f1994e378a5928da10f235fecc10ac0"
+app.config['SECRET_KEY'] = "5973598a5b9cba6ebb36d2f95019442d2f1994e378a5928da10f235fecc10ac0"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 CORS(app)
-database = { }
 
 
 # =====================================================================
 # Funtion sitess here, Create Funtion Here
 # =====================================================================
-def check_email(email):
-    for id , info in database.items():
-        if info["email"] == email:
-            return False
-    return True        
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50),  nullable=False)
+    email = db.Column(db.String(50) , nullable=False , unique=True)
+    password = db.Column(db.String(100), nullable=False) 
+
+with app.app_context():
+    db.create_all()
+
+
+# =====================================================================
+# Funtion sitess here, Create Funtion Here
+# =====================================================================
+            
 
 
 def check_password(p1 , p2):
@@ -32,7 +47,7 @@ def Create_token(id , name):
         "name" : name ,
         "exp" : time.time() + 1000
      }
-    token = jwt.encode(payload , SERVER_SECRET_KEY.encode() , algorithm='HS256')
+    token = jwt.encode(payload , app.config['SECRET_KEY'] , algorithm='HS256')
     return token
 
 
@@ -52,22 +67,20 @@ def check_login(l_email, l_password):
 def register():
     if request.method == "POST":
         data = request.get_json()
-        is_found = check_email(data["email"])
-        if not is_found:
+        existing_email = User.query.filter_by(email=data['email']).first()      
+        if existing_email:
             return jsonify({"status": "Error", "message": "Email Already Exists" }), 401
         else:
             is_pass = check_password(data["password"] , data["C_password"])
             if not is_pass:
-                return jsonify({"status": "Error", "message": "Password Already Exists" }), 401
+                return jsonify({"status": "Error", "message": "Password Doesen't Match" }), 401
             else:
-                id = len(database) + 1
-                database[id] = {
-                    "name" : data["name"] ,
-                    "email" : data["email"] ,
-                    "password" : data["password"],
-                    "C_password": data["C_password"]
-                }
-                token = Create_token(id , data["name"])
+                hashed_password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+                new_user = User(username=data['name'] , email=data['email'] , password=hashed_password)
+                db.session.add(new_user)
+                db.session.commit()
+                new_id = new_user.id
+                token = Create_token(new_id , data["name"])
 
                 return jsonify({"status": "success", 'token': token }), 200
 
@@ -83,13 +96,23 @@ def login():
         data = request.get_json()
         email = data['email']
         password = data['password']
-        is_found  , id , name= check_login(email , password)
-        if not is_found:
-            return jsonify({"status":"error" , "message": "Either email or password is incorrect"}) , 401
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if user:
+                typed_password_bytes = password.encode('utf-8')
+        
+                stored_hash_bytes = user.password.encode('utf-8')
+        
+                if bcrypt.checkpw(typed_password_bytes, stored_hash_bytes):  
+                    id = user.id
+                    token = Create_token(id , user.username)
+                    return jsonify({"status": "success", 'token': token }), 200
+                else:
+                    return jsonify({"status":"error" , "message": "Password incorrect"}) , 401    
         else:
-    
-            token = Create_token(id , name)
-            return jsonify({"status": "success", 'token': token }), 200
+            return jsonify({"status":"error" , "message": "Either email or password is incorrect"}) , 401
+
+            
 
 
  
@@ -107,11 +130,11 @@ def dashboard():
         return jsonify({"status": "error", "message": "Token is missing!"}), 401
 
     try:
-        data = jwt.decode(token, SERVER_SECRET_KEY.encode(), algorithms=['HS256'])
-        id = data['id']
+        data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+        l_user = User.query.get(data['id'])
         return jsonify({
             "status": "success",
-            "secret_data": f"Welcome {database[id]['name']}! Here is your highly secure data."
+            "secret_data": f"Welcome {l_user.username}! Here is your highly secure data."
         }), 200
 
     except jwt.ExpiredSignatureError:
